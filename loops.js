083f0002e9625bbb0ddd4c00e6d0020e61f71a7f
okuhlejuly <okuhlejuly2@gmail.com>
2025-11-05 08:25:38 +0200

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,26 @@
+const numbers = [1, 2, 3, 4, 5]
+
+for (number of numbers) {
+    console.log(number);
+}
+
+i = 5
+
+while (i > 0) {
+    console.log(i)
+    i -= 1
+}
+
+for (number of numbers) {
+    if (number % 2 === 0) {
+        console.log(number);
+    }    
+}
+
+let sum = 0;
+
+for (number of numbers) {
+    sum += number;
+}
+
+console.log(sum)
